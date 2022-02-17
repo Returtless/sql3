f551ad396220a4f626e827f7430df42df60b24e3
Vladislav Likhachev <mymindissharp@gmail.com>
2022-02-17 22:25:37 +0300

    initial commit

---
+++ src/main/java/com/example/sql3/Sql3Application.java
@@ -0,0 +1,13 @@
+package com.example.sql3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sql3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sql3Application.class, args);
+    }
+
+}
